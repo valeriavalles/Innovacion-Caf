317c68c6bf02b9a0b19416b1b4e3f6a93dc8ff95
valeriavalles <valeria.sv@outlook.es>
2018-05-26 11:45:36 -0500

    component botons and presents

---
+++ src/components/present.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class Presents extends Component {
+    render(){
+        return (
+            <div>
+                <figure>
+                    imagen 
+                </figure>
+                <div>
+                    Descripcion
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Presents; 
